4f98573acd8b42b2037472f5ca27ac8c6976ea3a
Fayyadh317 <132419776+Fayyadh317@users.noreply.github.com>
2025-12-07 05:31:01 +0800

    Tambah Nota [071225_05:30 AM]

--- tambah.php
+++ tambah.php
@@ -19,17 +19,25 @@
             include("config.php");
 
             if(isset($_POST['submit'])){
-            //
-
-                $attribute_1 = $_POST['attribute_1_input'];
+            // Jika ditetapkan ada $_POST dari input type yang bernama "submit" - Terjemahan kod if(isset($_POST['submit']))
+            // Bermakna, PHP bawah ni akan jalan kalau ada tindak balas pada input type bernama "submit"
+            // Dalam kes ni, bermakna kalau pengguna klik butang HANTAR tu, PHP untuk simpan kat bawah ni akan dijalankan
+                
+                //Pastikan nama kat dalam $_POST[''] sama dengan nama dekat HTML name=""
+                //Fungsi variable ni adalah pegang nilai yang pengguna input kat input type
+                
+                $attribute_1 = $_POST['attribute_1_input']; 
                 $attribute_2 = $_POST['attribute_2_input'];
 
                 $query = mysqli_query($connect,"INSERT INTO table_1 SET
                                                 attribute_1 = '".$attribute_1."',
                                                 attribute_2 = '".$attribute_2."'");
+                                                // Pastikan kat sini, nama atribut sama macam dalam database
+                                                // Atribut_IkutDB = '".$variable_InputPengguna."'");
 
                 header("location:index.php");
             }
         ?>
     </body>
+
 </html>
